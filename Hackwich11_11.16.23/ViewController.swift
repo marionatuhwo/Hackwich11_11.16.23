6a51f1a0d24361624d87551e2dae52a28b63e46b
marionatuhwo <ano@hawaii.edu>
2023-11-16 13:24:22 -1000

    Completed Parts 1-7 in class assignment

--- Hackwich11_11.16.23/ViewController.swift
+++ Hackwich11_11.16.23/ViewController.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var slider: UISlider!
+    var currentValue: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
+    @IBAction func myGuessButtonPressed(_ sender: Any)
+    {
+        //Part 7
+        let message = "The value is: \(currentValue)"
+        
+        let alert = UIAlertController(title: "Hello World", message: message, preferredStyle:.alert)
+                       
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+                       
+        alert.addAction(action)
+                       
+        present(alert, animated: true, completion: nil)
+
+    }
+    
+    @IBAction func sliderHasMoved(_ sender: Any)
+    {
+    
+        print("The Value of the slider is:\(slider.value)")
+        //Part 6
+        currentValue = Int((slider.value))
+        
+        
+    
+    
+    }
+    
 }
 
